5aa45d912d7141cdfeaf44c342c71dca12497965
Sebastian <sebastian@desertbit.com>
2023-11-15 13:52:32 +0100

    rebuild everything

--- examples/full/api/api_orbit_gen.go
+++ examples/full/api/api_orbit_gen.go
@@ -5,12 +5,6 @@ import (
 	context "context"
 	errors "errors"
 	fmt "fmt"
-	io "io"
-	net "net"
-	strings "strings"
-	sync "sync"
-	time "time"
-
 	closer "github.com/desertbit/closer/v3"
 	oclient "github.com/desertbit/orbit/pkg/client"
 	codec "github.com/desertbit/orbit/pkg/codec"
@@ -18,6 +12,11 @@ import (
 	oservice "github.com/desertbit/orbit/pkg/service"
 	transport "github.com/desertbit/orbit/pkg/transport"
 	validator "github.com/go-playground/validator/v10"
+	io "io"
+	net "net"
+	strings "strings"
+	sync "sync"
+	time "time"
 )
 
 // Ensure that all imports are used.
@@ -46,7 +45,6 @@ var (
 func _encodeTimeDuration(d time.Duration) int64 {
 	return int64(d)
 }
-
 func _decodeTimeDuration(i int64) time.Duration {
 	return time.Duration(i)
 }
